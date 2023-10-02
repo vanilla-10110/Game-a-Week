5659ff96960b39a8625bab6c875a3f7385798397
vanilla-10110 <106131238+vanilla-10110@users.noreply.github.com>
2023-10-02 23:45:44 +1100

    worked with animations
    the (now 4) sword attack animations smooth(ish)ly transition between eachother and their respective idle states, attack 3 is a little buggy. next step it to make all idles transition to idle1 after one loop, should be easy just by adding a transition from them all to the first one.

--- Games/Jammin-Roguelike6/Assets/Scripts/attackHandler.cs
+++ Games/Jammin-Roguelike6/Assets/Scripts/attackHandler.cs
@@ -8,21 +8,50 @@ public class attackHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator animator;
-    //private Animation anim;
     public GameObject weaponHolder;
+
     void Start()
     {
         //anim = weaponHolder.GetComponent<Animation>();
         //GetChild(0).
     }
 
+    bool isPlaying(Animator anim, string stateName)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
+            anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+            return true;
+        else
+            return false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        Attack(animator);
+    }
+    private void Attack(Animator anim)
+    {
+        if (Input.GetMouseButton(0) && !isPlaying(animator, "swordAttack1") && !isPlaying(animator, "swordAttack2") && !isPlaying(animator, "swordAttack3") && !isPlaying(animator, "swordAttack4"))
         {
-            Debug.Log("uhhhh what???");
-            animator.Play("swordAttack");
+            AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo(0);
+            if (currentState.IsName("swordIdle1"))
+            {
+                animator.Play("swordAttack1");
+            }
+            else if (currentState.IsName("swordIdle2"))
+            {
+                animator.Play("swordAttack2");
+            }
+            else if (currentState.IsName("swordIdle3"))
+            {
+                animator.Play("swordAttack3");
+            }
+            else if (currentState.IsName("swordIdle4"))
+            {
+                animator.Play("swordAttack4");
+            }
+            
         }
     }
 }
