152261bca8af2c49526b9d4eb6ff8f18d0ff168d
vanilla-10110 <106131238+vanilla-10110@users.noreply.github.com>
2023-10-04 22:14:30 +1100

    polished animations & added staff with one spell
    polished the animations, they be smoother now. added the staff animation and made a fireball shoot when the event in the animation happens. also improved the attackHandler script.

--- Games/Jammin-Roguelike6/Assets/Scripts/attackHandler.cs
+++ Games/Jammin-Roguelike6/Assets/Scripts/attackHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,51 +8,109 @@ using UnityEngine.InputSystem;
 public class attackHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Animator animator;
+    Animator animator;
     public GameObject weaponHolder;
+    
 
     void Start()
     {
-        //anim = weaponHolder.GetComponent<Animation>();
+        //animator = weaponHolder.GetComponent<Animation>();
         //GetChild(0).
     }
 
-    bool isPlaying(Animator anim, string stateName)
+    string GetActiveWeaponName()
     {
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
-            anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
-            return true;
-        else
-            return false;
-    }
+        foreach (Transform child in weaponHolder.transform)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                //Debug.Log(child.name);
+                return child.name;
+            }
 
+        }
+
+        return "None";
+    }
+    Animator GetActiveWeaponAnimator()
+    {
+        foreach (Transform child in weaponHolder.transform)
+        {
+            if (child.gameObject.activeSelf)
+            {
+               //Debug.Log(child.name);
+                animator = child.GetComponent<Animator>();
+                return animator;
+            }
+        }
+        return null;
+    }
     // Update is called once per frame
     void Update()
     {
-        Attack(animator);
-    }
-    private void Attack(Animator anim)
-    {
-        if (Input.GetMouseButton(0) && !isPlaying(animator, "swordAttack1") && !isPlaying(animator, "swordAttack2") && !isPlaying(animator, "swordAttack3") && !isPlaying(animator, "swordAttack4"))
+        if (Input.GetMouseButton(0))
         {
-            AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo(0);
+            Attack();
+        }
+        
+    }
+    //public void ShootSpell()
+    //{
+    //    GameObject spawnedSpell = Instantiate(spells[spellIndex]);
+    //    spawnedSpell.transform.position = spellSpawnPoint.position;
+    //    spawnedSpell.GetComponent<Rigidbody>().velocity = spellSpawnPoint.forward * spellVelocity;
+    //    Destroy(spawnedSpell, 5);
+    //}
+
+    private void Attack()
+    {
+
+
+        if (GetActiveWeaponName() == "staff")
+        {
+            
+            AnimatorStateInfo currentState = GetActiveWeaponAnimator().GetCurrentAnimatorStateInfo(0);
+            if (currentState.IsName("staffIdle1"))
+            {
+                
+                GetActiveWeaponAnimator().Play("staffAttack");
+                //Rigidbody instantiatedProjectile = Instantiate(spells[spellIndex], spellSpawnPoint.position, spellSpawnPoint.rotation)
+                //    as Rigidbody;
+                //instantiatedProjectile.velocity = transform.InverseTransformDirection(spellSpawnPoint.forward * spellVelocity);
+                //
+            }
+            
+
+            
+            
+        }
+
+        if (GetActiveWeaponName() == "sword")
+        {
+            
+            int attackIndex = -1;
+            AnimatorStateInfo currentState = GetActiveWeaponAnimator().GetCurrentAnimatorStateInfo(0);
             if (currentState.IsName("swordIdle1"))
             {
-                animator.Play("swordAttack1");
+                attackIndex = 0;
             }
             else if (currentState.IsName("swordIdle2"))
             {
-                animator.Play("swordAttack2");
+                attackIndex = 1;
             }
             else if (currentState.IsName("swordIdle3"))
             {
-                animator.Play("swordAttack3");
+                attackIndex = 2;
             }
             else if (currentState.IsName("swordIdle4"))
             {
-                animator.Play("swordAttack4");
+                attackIndex = 3;
             }
-            
+
+            if (attackIndex != -1)
+            {
+                GetActiveWeaponAnimator().Play($"swordAttack{attackIndex + 1}");
+            }
         }
     }
 }
