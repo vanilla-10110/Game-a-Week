56aba9321b699aa0d3451e6deade7b2144824474
RealFreditor <137402340+Bungitor@users.noreply.github.com>
2023-10-05 16:09:28 +1100

    Continued dungeon generation
    continued the videos but it isnt working so ima do something

--- Games/Jammin-Roguelike6/Assets/Scripts/dungeonSpawning/dungeonCreator.cs
+++ Games/Jammin-Roguelike6/Assets/Scripts/dungeonSpawning/dungeonCreator.cs
@@ -8,7 +8,7 @@ public class Dungeon : MonoBehaviour
     public int roomWidthMin, roomLengthMin;
     public int corridorWidth;
     public int maxIterations;
-    
+    public Material material;
 
 
 
@@ -23,12 +23,41 @@ public class Dungeon : MonoBehaviour
     {
         DungeonGenerator generator = new DungeonGenerator(dungeonWidth, dungeonLength);
         var listOfRooms = generator.CalculateRooms(maxIterations, roomWidthMin, roomLengthMin);
+        for (int i = 0; i < listOfRooms.Count; i++)
+        {
+            CreateMesh(listOfRooms[i].BottomLeftAreaCorner, listOfRooms[i].TopRightAreaCorner);
+        }
     }
 
-
-    // Update is called once per frame
-    void Update()
+    private void CreateMesh(Vector2 bottomLeftCorner, Vector2 topRightCorner)
     {
-        
+        Vector3 bottomLeftV = new Vector3(bottomLeftCorner.x, 0, bottomLeftCorner.y);
+        Vector3 bottomRightV = new Vector3(topRightCorner.x, 0, bottomLeftCorner.y);
+        Vector3 topLeftV = new Vector3(bottomLeftCorner.x, 0, topRightCorner.y);
+        Vector3 topRightV = new Vector3(topRightCorner.x, 0, topRightCorner.y);
+
+        Vector3[] vertices = new Vector3[]{topLeftV, topRightV, bottomLeftV, bottomRightV};
+        Vector2[] uvs = new Vector2[vertices.Length];
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(vertices[i].x , vertices[i].z);
+        }
+
+        int[] triangles = new int[] { 0, 1, 2, 2, 1, 3 };
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+
+        GameObject dungeonFloor = new GameObject("Mesh" + bottomLeftCorner, typeof(MeshFilter), typeof(MeshRenderer));
+
+        dungeonFloor.transform.position = Vector3.zero;
+        dungeonFloor.transform.localScale = new Vector3(1, 1, 1);
+        dungeonFloor.GetComponent<MeshFilter>().mesh = mesh;
+        dungeonFloor.GetComponent<MeshRenderer>().material = material;
+
+
+
+
     }
 }
