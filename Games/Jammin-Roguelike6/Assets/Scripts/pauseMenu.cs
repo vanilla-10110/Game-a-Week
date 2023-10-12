795d9bc8da6579e55f6fccc740cb9e5bb6b1bf19
RealFreditor <137402340+Bungitor@users.noreply.github.com>
2023-10-12 15:57:03 +1100

    Made menus
    Made pause menu and settings menu

---
+++ Games/Jammin-Roguelike6/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    
+    public GameObject pauseMenuUI;
+    public GameObject canvas;
+    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+                ReduceSound();
+            }
+            else
+            {
+                Pause();
+                IncreaseSound();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        canvas.SetActive(true);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        canvas.SetActive(false);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    void ReduceSound()
+    {
+        //reduce sound here
+    }
+    void IncreaseSound()
+    {
+        //make it normal again here
+    }
+
+
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("TitleAndMenus");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
