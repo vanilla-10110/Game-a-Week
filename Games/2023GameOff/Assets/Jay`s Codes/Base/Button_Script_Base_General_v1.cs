037781bcadf80df3fa319b66014e9bbd41371675
Jay <jay.programming350@gmail.com>
2023-11-09 20:21:54 +0100

    Added Main Menu
    Added main menu with options (still wip)

--- Games/2023GameOff/Assets/Jay`s Codes/Base/Button_Script_Base_General_v1.cs
+++ Games/2023GameOff/Assets/Jay`s Codes/Base/Button_Script_Base_General_v1.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Button_Script_Base_General_v1 : MonoBehaviour
 {
+    //private GameObject Base_Sprite;
     public GameObject RD_Screen;
     public GameObject Hangar_Screen;
     public GameObject Fuel_Management_Screen;
 
     private void Start()
     {
-        SceneManager.UnloadScene("Game");
         RD_Screen.SetActive(false);
         Hangar_Screen.SetActive(false);
         Fuel_Management_Screen.SetActive(false);
@@ -42,7 +43,8 @@ public class Button_Script_Base_General_v1 : MonoBehaviour
     public void Screen_manager_Launch_to_Asteroid_Field()
     {
         SceneManager.LoadScene("Game");
-        SceneManager.UnloadScene("Base");
+        //SceneManager.UnloadScene("Base");
     }
+
     //This Script is property of the Glorius USSR(1945)//
 }
