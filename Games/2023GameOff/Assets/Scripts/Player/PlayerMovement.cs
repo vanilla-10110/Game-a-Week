b40fa04813c109b8071708696e8ddd0629c12238
super-miner <theredmugproductions@gmail.com>
2023-11-14 20:14:52 -0800

    Adjusted player controls and added a max velocity to the player

--- Games/2023GameOff/Assets/Scripts/Player/PlayerMovement.cs
+++ Games/2023GameOff/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D _playerRB;   
-    private Vector2 _playerForces = Vector2.zero;
-
-
+    private Rigidbody2D _rb;
+    private Vector2 _forces;
+    
     [Header("Thruster Stats")]
     public float thrustForce = 75.0f;
+    public float maxSpeed = 30.0f;
     public float fuelEfficiency = 1.0f;
     public float fuelCapacity = 100.0f;
     public bool autoBrake = false;
 
     void Start()
     {
-        _playerRB = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
-
     
     void Update()
     {
@@ -27,7 +26,10 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        _playerForces = new Vector2(Input.GetAxisRaw("Horizontal") * thrustForce * Time.deltaTime, Input.GetAxisRaw("Vertical") * thrustForce * Time.deltaTime);
-        _playerRB.AddForce(_playerForces);
+        _forces = new Vector2(Input.GetAxisRaw("Horizontal") * thrustForce * Time.deltaTime, Input.GetAxisRaw("Vertical") * thrustForce * Time.deltaTime);
+        _rb.AddForce(_forces);
+        
+        _rb.velocity = Vector2.ClampMagnitude(_rb.velocity, maxSpeed);
+        Debug.Log(_rb.velocity);
     }
 }
