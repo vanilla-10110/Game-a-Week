9075ae0f20e59dc25e1c297be5414f64697c5b7c
super-miner <theredmugproductions@gmail.com>
2023-11-14 20:29:16 -0800

    Fixed a problem I caused with the thruster

--- Games/2023GameOff/Assets/Scripts/Player/ArmControls.cs
+++ Games/2023GameOff/Assets/Scripts/Player/ArmControls.cs
@@ -9,14 +9,14 @@ public class ArmControls : MonoBehaviour
     private Vector2 lastVelocity = Vector2.zero;
     private Vector2 currentAcceleration = Vector2.zero;
     private Rigidbody2D rb;
-    private GameObject _arms;
-    private GameObject _thruster;
+    private Transform _arms;
+    private Transform _thruster;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        _arms = GameObject.Find("Arms");
-        _thruster = GameObject.Find("Thruster");
+        _arms = transform.Find("Arms");
+        _thruster = transform.Find("Thruster");
     }
 
     private void Update()
@@ -24,7 +24,6 @@ public class ArmControls : MonoBehaviour
         //might combine into a single rotate function
         RotateThruster();
         RotateArms();
-        
     }
 
     private void RotateThruster()
@@ -33,7 +32,7 @@ public class ArmControls : MonoBehaviour
         currentAcceleration = (rb.velocity - lastVelocity) / Time.deltaTime;
         float angle = Mathf.Atan2(currentAcceleration.y, currentAcceleration.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        _thruster.transform.rotation = Quaternion.Slerp(_thruster.transform.rotation, rotation, thrusterRotateSpeed * Time.deltaTime);
+        _thruster.rotation = Quaternion.Slerp(_thruster.rotation, rotation, thrusterRotateSpeed * Time.deltaTime);
     }
 
     private void RotateArms()
@@ -45,6 +44,6 @@ public class ArmControls : MonoBehaviour
         Vector2 direction = mousePosition - objectScreenPosition;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-        _arms.transform.rotation = Quaternion.Slerp(_arms.transform.rotation, rotation, armsRotateSpeed * Time.deltaTime);
+        _arms.rotation = Quaternion.Slerp(_arms.rotation, rotation, armsRotateSpeed * Time.deltaTime);
     }
 }
