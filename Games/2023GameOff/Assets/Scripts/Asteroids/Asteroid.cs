b8e14aab6517f0f79fb505cd8bd9148abdddf1d8
Donutask <51319000+Donutask@users.noreply.github.com>
2023-11-20 09:27:05 +1100

    Made amogus rock rarer
    1 in 100

--- Games/2023GameOff/Assets/Scripts/Asteroids/Asteroid.cs
+++ Games/2023GameOff/Assets/Scripts/Asteroids/Asteroid.cs
@@ -8,13 +8,23 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     [SerializeField] Sprite[] sprites;
+    [SerializeField] Sprite susSprite;
     private SpriteRenderer spriteRenderer;
 
     public void Awake()
     {
-        //give random appearance
         spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+
+        //1 in 100 chance for amogus asteroid
+        if (Random.Range(0, 100) == 1)
+        {
+            spriteRenderer.sprite = susSprite;
+        }
+        //otherwise choose a random rock
+        else
+        {
+            spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        }
 
         //create collider after sprite change to ensure the hitbox matches visuals.
         //Do not add collider to sprite before object creation or things could go breaky
