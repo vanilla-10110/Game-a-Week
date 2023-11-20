eb1110ec37befced06538dc04966c2991b2a3120
Donutask <51319000+Donutask@users.noreply.github.com>
2023-11-20 11:51:03 +1100

    Lots of asteroid stuff
    Can target asteroids
    If asteroid is targeted, click to shoot laser
    Laser damages asteroid
    Enough damage destroys asteroid
    Some asteroids are 'valuable' and contain minerals. These have a shine particle effect. Destroying these asteroids spawns a placeholder mineral object
    Asteroids have random velocity and angular velocity.
    More asteroids spawn.
    
    Particles are place holders
    Need sounds
    Need beter selection indication (just tint sprite atm)
    Need damage effects
    Need to implement resources

---
+++ Games/2023GameOff/Assets/Scripts/Asteroids/Mineral.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mineral : MonoBehaviour
+{
+    /// <summary>
+    /// Name of mineral. Other properties could be added later
+    /// </summary>
+    public string mineralName;
+
+    //If touching player
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PickUp();
+        }
+    }
+
+    /// <summary>
+    /// Mineral is collected. Replace with whatever is needed
+    /// </summary>
+    void PickUp()
+    {
+        Debug.Log("Collected " + mineralName);
+        Destroy(gameObject);
+    }
+}
