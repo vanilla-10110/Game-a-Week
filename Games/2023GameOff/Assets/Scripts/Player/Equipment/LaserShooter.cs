18055bc8f6a2e3d6f0e494ceb4962a384f305618
vanilla-10110 <106131238+vanilla-10110@users.noreply.github.com>
2023-11-26 11:01:56 +1100

    added more ores and tried to improve grabber
    grabber should add force in direction of ship now yayyy

--- Games/2023GameOff/Assets/Scripts/Player/Equipment/LaserShooter.cs
+++ Games/2023GameOff/Assets/Scripts/Player/Equipment/LaserShooter.cs
@@ -85,7 +85,7 @@ public class LaserShooter : MonoBehaviour
             {
                 target = Asteroid.selectedAsteroid.transform.position;
                 //Debug.Log(laserDamage * Time.deltaTime);
-                Asteroid.selectedAsteroid.Damage(laserDamage);
+                Asteroid.selectedAsteroid.Damage(laserDamage * Time.deltaTime);
 
                 powerAmount -= powerEfficiency * Time.deltaTime * 2;
 
@@ -130,7 +130,9 @@ public class LaserShooter : MonoBehaviour
                     selection = Mineral.selectedMineral.gameObject;
                 else selection = SpaceObject.selectedAsteroid.gameObject;
                 target = selection.transform.position;
-                selection.GetComponent<Rigidbody2D>()./*velocity = */AddForce((_grabberPoint.transform.position - selection.transform.position).normalized * grabberForce);
+                selection.GetComponent<Rigidbody2D>().AddForce((new Vector2(_grabberPoint.transform.position.x, _grabberPoint.transform.position.y) - new Vector2(selection.transform.position.x, selection.transform.position.y)).normalized * (grabberForce + selection.GetComponent<Rigidbody2D>().mass) + gameObject.GetComponent<Rigidbody2D>().velocity * selection.GetComponent<Rigidbody2D>().mass);
+
+                //selection.GetComponent<Rigidbody2D>().AddForce((_grabberPoint.transform.position - selection.transform.position).normalized * (grabberForce + selection.GetComponent<Rigidbody2D>().mass) + gameObject.GetComponent<Rigidbody2D>().velocity);
 
                 powerAmount -= powerEfficiency * Time.deltaTime;
 
