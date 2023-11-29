63c4c23dc0eb223adb51826796a35654e0b3a4e8
super-miner <theredmugproductions@gmail.com>
2023-11-28 19:03:59 -0800

    Added options menu to pause screen

--- Games/2023GameOff/Assets/Scripts/UI/Menus/PauseMenu.cs
+++ Games/2023GameOff/Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour {
+    [SerializeField] private GameObject defaultOnMenu;
+    [SerializeField] private GameObject defaultOffMenu;
     [SerializeField] private GameObject menu;
     
     private void Update() {
@@ -12,5 +14,10 @@ public class PauseMenu : MonoBehaviour {
     public void ToggleMenu() {
         menu.SetActive(!menu.activeSelf);
         Time.timeScale = Time.timeScale > 0.0f ? 0.0f : 1.0f;
+
+        if (menu.activeSelf) {
+            defaultOnMenu.SetActive(true);
+            defaultOffMenu.SetActive(false);
+        }
     }
 }
